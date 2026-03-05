f29184c74405e075fbd496a52861cd2cbc0c73ed
unknown <jmsanmartin@espe.edu.ec>
2026-03-04 22:40:31 -0500

    All fixed

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Obtener perfil (RF#005)
 router.get('/profile/:id', getUserProfile);
 
-// Actualizar perfil (RF#003) - Se usa upload.single para la imagen de perfil
+// Actualizar perfil (RF#003) - Se usa upload.single para la imagen de perfil lo cual si se cumple
 router.put('/profile/:id', upload.single('foto_perfil'), updateUserProfile);
 
 export default router;
